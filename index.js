642d7c945873a73c92d241810eccc672796520a2
steven dulworth <crowscoupetech@proton.me>
2023-10-07 13:17:47 -0500

    learnig to fetch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            let body = document.querySelector('body');
+            let p = document.createElement('p');
+            p.textContent = object.id;
+            body.appendChild(p);
+        })
+        .catch(function(error) {
+            let body = document.querySelector('body');
+            let p = document.createElement('p');
+            p.textContent = error.message;
+            body.appendChild(p);
+        });
+}
